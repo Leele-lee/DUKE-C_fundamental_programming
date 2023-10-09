1aa78d30baa841a3cb5df3f565ef90528553b743
Coursera Learner <nobody@nowhere.nul>
2023-10-09 03:34:27 +0000

    find !

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -8,7 +8,7 @@
 //   will be 3.  ptr will point at an existing placeholder card
 //   (it will point into a hand at a card which was added
 //   with add_empty_card).
-/*void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
+void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
   if (fc == NULL) {
     fc = malloc(sizeof(*fc));
     fc->decks = NULL;
@@ -49,56 +49,8 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
     }
   }
 }
-*/
 
-void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
-  if(fc==NULL){
-    fc=malloc(sizeof(*fc));
-    assert(fc!=NULL);
-    fc->decks = malloc(sizeof(*fc->decks));
-    fc->n_decks=0;
-  }
 
-  if(fc->n_decks < index+1){
-    fc->decks=realloc(fc->decks,(index+1)*sizeof(*fc->decks));
-    assert(fc->decks!=NULL);
-    for(size_t i=fc->n_decks;i<index+1;i++){
-      fc->decks[i].cards=NULL;
-      fc->decks[i].n_cards=0;
-    }
-    fc->n_decks=index+1;
-  }
 
-  fc->decks[index].cards=realloc(fc->decks[index].cards,((fc->decks[index].n_cards)+1)*sizeof(*fc->decks[index].cards));
-  assert(fc->decks[index].cards!=NULL);
-  fc->decks[index].n_cards++;
-  fc->decks[index].cards[fc->decks[index].n_cards-1]=ptr;
-}
-
-void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
-  if(deck==NULL){
-    fprintf(stderr,"Error: Ivalid deck!\n");
-    assert(deck!=NULL);
-  }
-  if(fc==NULL){
-    fprintf(stderr,"Error: Ivalid future cards deck!\n");
-    assert(fc!=NULL);
-  }
-  if(deck->n_cards<fc->n_decks){
-    fprintf(stderr,"Error: Deck too small\n");
-    assert(deck->n_cards>=fc->n_decks);
-  }
-  for (size_t i=0; i<fc->n_decks; i++) { 
-    if(fc->decks[i].n_cards==0){
-      fprintf(stderr,"Error: Invalid future card deck at index '%ld'\n",i);
-      assert(fc->decks[i].n_cards!=0);
-    }
-    if (fc->decks[i].cards != NULL) {
-      for (size_t j=0; j<fc->decks[i].n_cards; j++) {
-	*(fc->decks[i].cards[j]) = *(deck->cards[i]);
-      }
-    }
-  }
-}
 
 
--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -54,7 +54,53 @@
 
 
 
-/*deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
+
+
+
+deck_t * hand_from_string(const char * str, future_cards_t * fc){
+  deck_t * deck=malloc(sizeof(*deck));
+  deck->cards=NULL;
+  deck->n_cards=0;
+
+  // analyse imported line
+  for (int i=0; i<strlen(str); i++){
+
+    // ignore characters space or newline
+    if(str[i] == ' ' || str[i] == '\n') continue;
+    
+    // if card is not unknown, add it
+    if(str[i] != '?') {
+      card_t card = card_from_letters(str[i], str[i+1]);
+      add_card_to(deck,card);
+      i++;
+    }else{
+      // if card is unknown add a future card
+      char str_n[strlen(str)];
+      int n=0;
+      i++;
+
+      // copy all card characters to the string up to next newline or space
+      while (!(str[i] == ' '|| str[i] == '\n' )){
+	str_n[n]=str[i];
+	i++;
+	n++;
+      }
+      // finish card string with terminator
+	str_n[n]= '\0';
+	add_future_card(fc, atoi(str_n), add_empty_card(deck));
+      }
+    }
+
+  // hands have to be minimum 5 cards
+  if (deck->n_cards < 5) {
+    fprintf(stderr, "Hands smaller than 5 cards");
+    return NULL;
+  }
+  return deck;
+
+}
+
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
   assert(f != NULL);
   deck_t ** ans = NULL;
   char * line = NULL;
@@ -77,88 +123,3 @@
   free(line);
   return ans;
 }
-*/
-
-deck_t * hand_from_string(const char * str, future_cards_t * fc) {
-  deck_t *cur_deck = NULL;
-  cur_deck = malloc(sizeof(*cur_deck));
-  cur_deck->n_cards = 0;
-  cur_deck->cards = NULL;
-  char *p=strchr(str,'\n');
-  if(p!=NULL){
-    *p='\0';
-  }
-  for (size_t i=0; i<strlen(str)+1; i++) {
-    if (str[i+1] == '\0' || str[i] == '\0') {
-      break;
-    }
-    if (str[i+1] == ' ' || str[i] == ' ') {
-      continue;
-    }
-      
-    if ((isdigit(str[i]) || str[i]=='A' || str[i]=='K' || str[i]=='Q' || str[i]=='J') && (str[i+1]=='s' || str[i+1]=='h' || str[i+1]=='d' || str[i+1]=='c')) {
-      card_t card = card_from_letters(str[i], str[i+1]);
-      add_card_to(cur_deck, card);
-    }
-    else if (str[i] == '?' && isdigit(str[i+1])) {
-      
-      char *number=malloc(2*sizeof(*number));
-      assert(number!=NULL);
-      size_t j=0;
-      number[j++]=str[i+1];
-      number[j]='\0';
-      if(isdigit(str[i+2])){
-      	number=realloc(number,3*sizeof(*number));
-      	assert(number!=NULL);
-      	number[j++]=str[i+2];
-      	number[j]='\0';
-      	i++;
-      }
-      add_future_card(fc,atoi(number),add_empty_card(cur_deck));
-      free(number);
-    }
-    else {
-      fprintf(stderr,"Error: Invalid input file format, line %s",str);
-      card_from_letters(0,0);
-    }
-  }
-  return cur_deck;
-}
-
-deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc) {
-  char * line = NULL;
-  size_t sz = 0;
-  deck_t ** deck = NULL;
-  if (f == NULL) {
-    perror("Error: Could not read file");
-    exit(EXIT_FAILURE);
-  }
-  
-  while (getline(&line, &sz, f) >= 0) {
-    char *p=strchr(line,'\n');
-    if(p!=NULL){
-      *p='\0';
-    }
-    else{
-      fprintf(stderr,"Error: Invalid line '%s' in input file!\n",line);
-      assert(p!=NULL);
-    }
-    
-    deck = realloc(deck, ((*n_hands) + 1)* sizeof(*deck));
-    assert(deck != NULL);
-    (*n_hands)++;
-    deck[(*n_hands)-1] =  hand_from_string(line, fc);
-    
-    if(deck[(*n_hands)-1]->n_cards<5){
-      fprintf(stderr,"Error: Invalid number of cards in input file\n");
-      exit(EXIT_FAILURE);
-    }
-    free(line);
-    
-    sz = 0;
-  }
-  free(line);
-
-  return deck;
-}
-
